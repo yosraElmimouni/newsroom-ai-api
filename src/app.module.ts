cec309d9cc0d9aa5acf3ae43013cea20e7c57a32
YosraElmimouni <yosraelmimouni2003@gmail.com>
2026-05-17 08:36:06 +0200

    configure neon database

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,26 +10,45 @@ import { MediaModule } from './media/media.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync(
-      {
-        imports: [ConfigModule],
-        inject:[ConfigService],
-        useFactory: (configservice: ConfigService)=> ({
-          type:'postgres',
-          host:configservice.get('DB_HOST'),
-          port: parseInt(configservice.get('DB_PORT') || '5432'),
-          username:configservice.get('DB_USERNAME'),
-          password:configservice.get('DB_PASSWORD'),
-          database:configservice.get('DB_NAME'),
-          entities:[join(__dirname, '**', '*.entity.{ts,js}')],
-          synchronize:true
-        })
-      }
+    TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+
+  useFactory: (configService: ConfigService) => ({
+
+    type: 'postgres',
+
+    host: configService.get('DB_HOST'),
+
+    port: parseInt(
+      configService.get('DB_PORT') || '5432'
     ),
+
+    url: configService.get('DATABASE_URL'),
+
+    username: configService.get('DB_USERNAME'),
+
+    password: configService.get('DB_PASSWORD'),
+
+    database: configService.get('DB_NAME'),
+
+    ssl: {
+      rejectUnauthorized: false,
+    },
+
+    entities: [
+      join(__dirname, '**', '*.entity.{ts,js}')
+    ],
+
+    synchronize: true,
+  }),
+}),
     ArticleModule,
     MediaModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
